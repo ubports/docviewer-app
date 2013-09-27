5549a447a8f898f5af7302bf2593beaa9da23009
Carla Sella <carla.sella@gmail.com>
2013-09-27 21:06:14 +0200

    WIP

---
+++ tests/autopilot/ubuntu_docviewer_app/emulators.py
@@ -0,0 +1,30 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2013 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+from ubuntuuitoolkit import emulators as toolkit_emulators
+
+
+class MainView(toolkit_emulators.MainView):
+
+
+    def get_object(self, typeName, name):
+        """Get a specific object"""
+        return self.app.select_single(typeName, objectName=name)
+
+    def get_objects(self, typeName, name):
+        """Get more than one object"""
+        return self.app.select_many(typeName, objectName=name)
