fa4fa7f763a0e220d57484711a111ce6b719c73e
David Planella <david.planella@ubuntu.com>
2014-10-21 13:45:46 -0400

    Fixed pep8 issues

--- tests/autopilot/ubuntu_docviewer_app/tests/__init__.py
+++ tests/autopilot/ubuntu_docviewer_app/tests/__init__.py
@@ -34,13 +34,13 @@ class DocviewerTestCase(AutopilotTestCase):
     def setUp(self):
         self.binary = 'ubuntu-docviewer-app'
         self.source_dir = os.path.dirname(
-                   os.path.dirname(os.path.abspath('.')))
+            os.path.dirname(os.path.abspath('.')))
         self.build_dir = self._get_build_dir()
 
         self.local_location = self.build_dir
         self.local_location_qml = os.path.join(self.build_dir,
-                                               'src', 'app',
-                                               'qml', 'ubuntu_docviewer_app.qml')
+                                               'src', 'app', 'qml',
+                                               'ubuntu_docviewer_app.qml')
         self.local_location_binary = os.path.join(self.build_dir,
                                                   'src', 'app', self.binary)
         self.installed_location_binary = os.path.join('/usr/bin/', self.binary)
